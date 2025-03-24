310b7e7661bfaca36c952d0d279a096205ed6970
Maksym Rutkovskyi <maxikrud0071@gmail.com>
2025-03-24 19:26:03 +0200

    task 1,2,3,4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+    constructor(
+      private signature: number = Math.random()
+    ) {
+    }
+
+    getSignature(): number {
+      return this.signature;
+    }
+}
+
+class Person {
+  constructor(
+    private key: Key
+  ) {
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(
+    protected key: Key
+  ) {
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Welcome');
+    } else {
+      console.log('No way');
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door open');
+    } else {
+      console.log('Door closed');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
